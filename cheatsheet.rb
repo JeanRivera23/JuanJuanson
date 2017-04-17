51dbfcabfd6d70493afee1cadd23ac51dc60e3b8
Jeanette Rivera <jeanetterivera465@gmail.com>
2017-04-17 15:01:24 -0400

    some changes

--- cheatsheet.rb
+++
@@ -1,4 +0,0 @@
-# Directions:
-# Anytime you see code, follow along in terminal
-# Create a cheatsheet from Lesson Objectives and submit github link of file to slack when done
-# Notify Liza via slack when done so you can get the lab
