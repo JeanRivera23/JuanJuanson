459cf60d9323adf14416e1aab71b82a79d578012
Jeanette Rivera <jeanetterivera465@gmail.com>
2017-04-17 14:54:56 -0400

    first commit, directions

---
+++ cheatsheet.rb
@@ -0,0 +1,4 @@
+# Directions:
+# Anytime you see code, follow along in terminal
+# Create a cheatsheet from Lesson Objectives and submit github link of file to slack when done
+# Notify Liza via slack when done so you can get the lab
